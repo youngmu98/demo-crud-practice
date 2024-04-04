3c51a59110d6da77187088947926e39ccb891840
dbdudann <dbdudann@gmail.com>
2024-04-04 13:07:27 +0900

    아티클 목록조회

--- src/main/java/com/example/demo/controller/BlogApiController.java
+++ src/main/java/com/example/demo/controller/BlogApiController.java
@@ -2,14 +2,18 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Article;
 import com.example.demo.dto.AddArticleRequest;
-import com.example.demo.repository.BlogRepository;
+import com.example.demo.dto.ArticleResponse;
 import com.example.demo.service.BlogService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +22,23 @@ public class BlogApiController {
     private final BlogService blogService;
 
     @PostMapping("/api/articles")
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+
+        return ResponseEntity.ok().body(articles);
+    }
+
 }
--- src/main/java/com/example/demo/service/BlogService.java
+++ src/main/java/com/example/demo/service/BlogService.java
@@ -6,13 +6,19 @@ import com.example.demo.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BlogService {
 
     private final BlogRepository blogRepository;
 
-    public Article save(AddArticleRequest request){
+    public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
