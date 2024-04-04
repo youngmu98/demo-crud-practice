1614c4aa41c00a03b110c96c13dec6eaf64ee60e
dbdudann <dbdudann@gmail.com>
2024-04-04 13:07:43 +0900

    샘플 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+-- member 테이블 임시 데이터 생성 --
 INSERT INTO member (id, name) VALUES (1, 'name 1');
 INSERT INTO member (id, name) VALUES (2, 'name 2');
 INSERT INTO member (id, name) VALUES (3, 'name 3');
+
+-- article 테이블 임시 데이터 생성 --
+INSERT INTO article (title, content) VALUES ('제목1', '내용1');
+INSERT INTO article (title, content) VALUES ('제목2', '내용2');
+INSERT INTO article (title, content) VALUES ('제목3', '내용3');
